d518f23e37ebd3e8114ce92384caaba70b1f0411
Raju <rajuvaghela1485@gmail.com>
2026-04-12 22:43:32 +0530

    just completed prisma basics

--- prisma/index.js
+++ prisma/index.js
@@ -4,14 +4,42 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: "Raju",
-      email: "raju@gmail.com",
-    },
+  // insert single record
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "Raju",
+  //     email: "raju111@gmail.com",
+  //   },
+  // });
+
+  // insert multiple
+  //   const users = await prisma.user.createMany({
+  //   data: [
+  //     { name: "Alice1", email: "Alice10@gmail.com",},
+  //        { name: "Bob1", email: "bom10@gmail.com",}
+  //   ],
+  // });
+
+  //const users = await prisma.user.findMany();
+  // const users = await prisma.user.findUnique({
+  //   where: { id: 13 },
+  // });
+
+  // const updatedUser =await prisma.user.update({
+  //   where:{id:13},
+  //   data:{name:"Bob"}
+  // })
+
+  // const updatedUser = await prisma.user.updateMany({
+  //   where: { id: 13 },
+  //   data: { name: "Bob the builder" },
+  // });
+
+  const deletedUser = await prisma.user.delete({
+    where: { id: 12 },
   });
 
-  console.log(user);
+  console.log(deletedUser);
 }
 
 main()
